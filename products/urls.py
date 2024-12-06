1982bbd8fa052c48d04eabe3578ba074c6671903
Mostafa Hesham <mostafaasd2018@gmail.com>
2024-12-06 17:39:36 +0200

    Add Filters EndPoints Colors, Sizes, Categories, And Brands

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, SizeListView, ColorListView, CategoryListView, BrandListView
 
 app_name = 'products'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='products-list'),
     path('<int:pk>/', ProductDetailView.as_view(), name='products-detail'),
+    path('categories/', CategoryListView.as_view(), name='categories-list'),
+    path('brands/', BrandListView.as_view(), name='brands-list'),
+    path('colors/', ColorListView.as_view(), name='colors-list'),
+    path('sizes/', SizeListView.as_view(), name='sizes-list'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,10 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from products.serializers import ListProductSerializer, CreateProductSerializer, DetailProductSerializer
+from oauthlib.uri_validate import query
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+
+from products.models import VariantSize, VariantColor, Category, Brand
+from products.serializers import ListProductSerializer, CreateProductSerializer, DetailProductSerializer, \
+    VariantSizeSerializer, VariantColorSerializer, CategorySerializer, BrandSerializer
 from core.permissions import ReadOnly, IsSeller
 from products.filters import ProductFilter
 from products.services import get_product_queryset, get_seller_products_queryset
@@ -35,3 +39,30 @@ class ProductDetailView(RetrieveUpdateDestroyAPIView):
         seller = self.request.user
         queryset = get_seller_products_queryset(seller)
         return queryset
+
+class SizeListView(ListAPIView):
+    queryset = VariantSize.objects.all()
+    serializer_class = VariantSizeSerializer
+    permission_classes = [ReadOnly]
+
+class ColorListView(ListAPIView):
+    queryset = VariantColor.objects.all()
+    serializer_class = VariantColorSerializer
+    permission_classes = [ReadOnly]
+
+class CategoryListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [ReadOnly]
+
+class BrandListView(ListAPIView):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+    permission_classes = [ReadOnly]
+
+    def get_queryset(self):
+        queryset = Brand.objects.all()
+        print("queryset: ",queryset)
+        return queryset
+
+
