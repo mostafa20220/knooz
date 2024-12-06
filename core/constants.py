aa2d5ccd878b896127ff195af783ae89617b8489
Mostafa Hesham <mostafaasd2018@gmail.com>
2024-12-06 17:40:30 +0200

    Move More Constants to The File

--- core/constants.py
+++ core/constants.py
@@ -5,3 +5,13 @@ SHIPPING_FEE_THRESHOLD = Decimal('200')
 
 COD_FEE = Decimal('10')
 ADDED_VALUE_TAX_RATE = Decimal('0.14')
+
+CASH_ON_DELIVERY = 'cash on delivery'
+CREDIT_CARD= 'credit card'
+
+PAYMENT_METHOD_CHOICES = [
+    (CREDIT_CARD, 'Credit Card'),
+    (CASH_ON_DELIVERY, 'Cash on Delivery')
+]
+
+FREE = Decimal('0.00')
